22e45b8264f19d8ae04c28525371239966928c45
Paul Melnikow <github@paulmelnikow.com>
2017-12-05 17:46:25 -0500

    Optionally render children inline (#14)
    Fixes this React error, when ClickToSelect is used inside a <p> or other phrasing content.
    
    validateDOMNesting(...): <div> cannot appear as a descendant of <p>.
    
    A <span> is used by default, and the developer can specify containerElement="div" when they want a <div>.
    
    Fix #12

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import PropTypes from 'prop-types';
  * the text within.
  */
 class ClickToSelect extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+    containerElement: PropTypes.oneOf(['span', 'div']),
+  };
+
+  static defaultProps = {
+    containerElement: 'span',
+  };
+
   select = e => {
     e.preventDefault();
     const range = document.createRange();
@@ -21,16 +30,14 @@ class ClickToSelect extends React.PureComponent {
   };
 
   render() {
+    const Element = this.props.containerElement;
+
     return (
-      <div ref={this.getRef} onClick={this.select}>
+      <Element ref={this.getRef} onClick={this.select}>
         {this.props.children}
-      </div>
+      </Element>
     );
   }
 }
-
-ClickToSelect.propTypes = {
-  children: PropTypes.any.isRequired
-};
 
 export default ClickToSelect;
